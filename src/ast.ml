fb4f2f76ffc068dfcb2177dcdd836290cf8dec50
Gargi Sharma <gs051095@gmail.com>
2020-06-10 00:26:45 +0530

    Implement AST and some parser functions

--- src/ast.ml
+++ src/ast.ml
@@ -6,12 +6,48 @@ module type Node = sig
   val token_literal : t -> string
 end
 
-include Token
+module Statement : sig
+  include Node
 
-module Statement : Node = struct
+  val statement_node : t -> t
+end = struct
   type t = Token.token
 
   let token_literal t = Token.token_to_string t
+
+  let statement_node t = t
 end
 
-let program : (module Node) list = [ (module Statement) ]
+module Expression : sig
+  include Node
+
+  val expression_node : t -> t
+end = struct
+  type t = Token.token
+
+  let token_literal t = Token.token_to_string t
+
+  let expression_node t = t
+end
+
+module Identifier = struct
+  type t = { token : Token.token; value : string }
+end
+
+module LetStatement = struct
+  type t = { token : Token.token; name : Identifier.t; value : Expression.t }
+end
+
+module Node' = struct
+  type t = E of Expression.t | S of Statement.t
+end
+
+type program = Node'.t list
+
+(* type node = {token: Token.token} *)
+
+(* type identifier = {token: Token.token; value: string;} *)
+
+(* type expression = {node: node; token: Token.token; } *)
+
+(* type statement = {} *)
--- src/parser.ml
+++ src/parser.ml
@@ -2,20 +2,24 @@ include Token
 include Lexer
 
 module Parser = struct
-  type parser = {
+  type t = {
     lexer : Lexer.lexer;
     curr_token : Token.token;
     peek_token : Token.token;
   }
 
-let next_token =
-  let  parser.curr_token = parser.peek_token in
-   parser.peek_token = parser.lexer.next_token in
-   parser
+  let next_token t =
+    let next_lexer, next_token = Lexer.next_token t.lexer in
+    { lexer = next_lexer; curr_token = t.peek_token; peek_token = next_token }
 
+  let new_parser t =
+    let curr_lexer, cur_token = Lexer.next_token t.lexer in
+    let peek_lexer, next_token = Lexer.next_token curr_lexer in
+    { lexer = peek_lexer; curr_token = cur_token; peek_token = next_token }
 
-  let new_parser = {lexer: Lexer.lexer; curToken: next_token; peekToken: next_token}
-
-  let parse_program = ""
-
+  (*   let parse_let_statement t = match t.peek_token with
+   *     | Ident ident ->
+   *       Some (Ast.LetStatement )
+   *     | _ -> None
+   *)
 end
